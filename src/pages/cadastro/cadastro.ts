30301c2cb26acb45890f7499ee30eec3b134de6b
Renan Camillo - Note <rcamillo12@gmail.com>
2018-11-08 01:39:20 -0200

    Inplantação camera
    Cadastro completo -funcional
    
    **Perfil - Atualizar foto

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -1,21 +1,27 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ToastController} from 'ionic-angular';
+import { Component } from "@angular/core";
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  ToastController
+} from "ionic-angular";
 
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AngularFireAuth } from '@angular/fire/auth';
+import { Camera, CameraOptions } from "@ionic-native/camera";
 
-import { UsuarioProvider } from '../../providers/usuario/usuario';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { AngularFireAuth } from "@angular/fire/auth";
 
+import { UsuarioProvider } from "../../providers/usuario/usuario";
 
 @IonicPage()
 @Component({
-  selector: 'page-cadastro',
-  templateUrl: 'cadastro.html',
+  selector: "page-cadastro",
+  templateUrl: "cadastro.html"
 })
 export class CadastroPage {
-  title: string;
+  public fotoPerfil: any;
   form: FormGroup;
-  usuario: any;  
+  usuario: any;
 
   constructor(
     public navCtrl: NavController,
@@ -23,14 +29,40 @@ export class CadastroPage {
     private formBuilder: FormBuilder,
     private provider: UsuarioProvider,
     private toast: ToastController,
-    public afAuth: AngularFireAuth) 
-    {
-    this.usuario = this.navParams.data.usuario || { };
-    this.createForm();
+    public afAuth: AngularFireAuth,
+    public camera: Camera
+  ) {
+    this.usuario = this.navParams.data.usuario || {};
+    this.createForm(this.fotoPerfil);
   }
 
-  createForm() {
-    this.form = this.formBuilder.group({      
+  abreCamera() {
+    //options do role
+    const options: CameraOptions = {
+      destinationType: this.camera.DestinationType.DATA_URL,
+      targetWidth: 500,
+      targetHeight: 500,
+      allowEdit: true
+      //encodingType: this.camera.EncodingType.JPEG,
+      //mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.camera.getPicture(options).then(
+      imageData => {
+        // imageData is either a base64 encoded string or a file URI
+        // If it's base64 (DATA_URL):
+        let base64Image = "data:image/jpeg;base64," + imageData;
+        this.fotoPerfil = base64Image;
+        this.createForm(this.fotoPerfil);
+      },
+      err => {
+        // Handle error
+      }
+    );
+  }
+
+  createForm(imagem) {
+    this.form = this.formBuilder.group({
       key: [this.usuario.key],
       nome: [this.usuario.nome, Validators.required],
       email: [this.usuario.email, Validators.required],
@@ -38,25 +70,30 @@ export class CadastroPage {
       babyname: [this.usuario.babyname, Validators.required],
       sexo: [this.usuario.sexo, Validators.required],
       babyDate: [this.usuario.babyDate, Validators.required],
+      fotoPerfil: [imagem, Validators.required]
     });
   }
 
   onSubmit() {
     if (this.form.valid) {
-      this.provider.save(this.form.value)
+      this.provider
+        .save(this.form.value)
         .then(() => {
-          this.toast.create({ message: 'Usuario salvo com sucesso.', duration: 3000 }).present();
+          this.toast
+            .create({ message: "Usuario salvo com sucesso.", duration: 3000 })
+            .present();
           this.navCtrl.pop();
         })
-        .catch((e) => {
-          this.toast.create({ message: 'Erro ao salvar usuário.', duration: 3000 }).present();
+        .catch(e => {
+          this.toast
+            .create({ message: "Erro ao salvar usuário.", duration: 3000 })
+            .present();
           console.error(e);
-        })
+        });
     }
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CadastroPage');
+    console.log("ionViewDidLoad CadastroPage");
   }
-
 }
--- src/providers/usuario/usuario.ts
+++ src/providers/usuario/usuario.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-import { AngularFireDatabase } from 'angularfire2/database';
-import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireDatabase } from "angularfire2/database";
+import { AngularFireAuth } from "@angular/fire/auth";
+
+import {
+  AngularFireStorage,
+  AngularFireUploadTask
+} from "angularfire2/storage";
 /*
   Generated class for the UsuarioProvider provider.
 
@@ -14,34 +19,50 @@ export class UsuarioProvider {
 
   constructor(
     private db: AngularFireDatabase,
-    public afAuth: AngularFireAuth) 
-  {}
+    public afAuth: AngularFireAuth,
+    private afStorage: AngularFireStorage
+  ) {}
 
   save(usuario: any) {
     return new Promise((resolve, reject) => {
       if (usuario.key) {
-        this.db.list(this.PATH)
-          .update(usuario.key, {nome: usuario.nome, babyname: usuario.babyname, sexo: usuario.sexo, babyDate: usuario.babyDate})
+        this.db
+          .list(this.PATH)
+          .update(usuario.key, {
+            nome: usuario.nome,
+            babyname: usuario.babyname,
+            sexo: usuario.sexo,
+            babyDate: usuario.babyDate,
+            fotoPerfil: usuario.foto
+          })
           .then(() => resolve())
-          .catch((e) => reject(e));
+          .catch(e => reject(e));
       } else {
-        this.afAuth.auth.createUserWithEmailAndPassword(usuario.email, usuario.senha).then((resolve) => { 
-          let iduser = resolve.user.uid;
-          let key: string;
-          key = this.db.list(this.PATH).push({iduser: iduser, nome: usuario.nome, email: usuario.email, babyname: usuario.babyname, sexo: usuario.sexo, babyDate: usuario.babyDate}).key;
-          this.db.list(this.PATH)
-         .update(key, {key:key})
-          .catch((e) => reject(e));
-        })
-      .catch((error) => {
-      })
+        this.afAuth.auth
+          .createUserWithEmailAndPassword(usuario.email, usuario.senha)
+          .then(resolve => {
+            let iduser = resolve.user.uid;
+            let key: string;
+            key = this.db.list(this.PATH).push({
+              iduser: iduser,
+              nome: usuario.nome,
+              email: usuario.email,
+              babyname: usuario.babyname,
+              sexo: usuario.sexo,
+              babyDate: usuario.babyDate,
+              fotoPerfil: usuario.fotoPerfil
+            }).key;
+            this.db
+              .list(this.PATH)
+              .update(key, { key: key })
+              .catch(e => reject(e));
+          })
+          .catch(error => {});
       }
-    }
-    )
+    });
   }
 
   remove(key: string) {
     return this.db.list(this.PATH).remove(key);
   }
-
 }
